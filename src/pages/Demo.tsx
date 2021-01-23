3665e4d06b7c65d4b24a879c289358a1fec2d04c
Ayinla Olajide <ayinlaolajide@yahoo.com>
2021-01-23 08:46:27 +0100

    show static popup on demo page

--- src/pages/Demo.tsx
+++ src/pages/Demo.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { getPopup } from '../lib/popup';
 
 import './Demo.css';
 
-export const Demo = () => {
-  const popup = getPopup();
+
+export const Demo: React.FC = () => {
+  const [showPopup, setShowPopup] = useState(false);
+  const popup: popup = getPopup();
 
   /**
    * Step 1: Render popup message in an overlay
    */
-  return <div />;
+  return (
+    <div>
+      <div className={showPopup ? 'container' : 'containerHidden'}>
+        <div className={showPopup ? 'panel' : 'panelHidden'}>
+          <div className = 'modal'>
+            <h3 className='modalTitle'>{popup.message}</h3>
+            <a className='closeText' onClick={() => setShowPopup(false)}>Close</a>
+          </div>
+        </div>
+        <div className={showPopup ? 'mask' : 'maskHidden'} onClick={() => setShowPopup(false)} />
+      </div>
+      <h1 className='welcomeText' onClick={() => setShowPopup(true)}>Show Popup 🎉</h1>
+    </div>
+  );
 };
