cc6249aa4cb832881e24ed80115215f13458d3b8
Ayinla Olajide <ayinlaolajide@yahoo.com>
2021-01-23 09:25:05 +0100

    Add ability to fetch popup from fake rest API

--- src/lib/popup.ts
+++ src/lib/popup.ts
@@ -1,9 +1,9 @@
-export function getPopup() {
+import { getPopupData } from '../services/api';
+export async function getPopup() {
   /**
    * Step 2: Instead of directly returning the popup definition fetch it from http://localhost:8001/popup
    */
-  return {
-    id: 0,
-    message: 'Hello, WisePops!',
-  };
+
+  const response = await getPopupData();  
+  return response.data;
 }
